0c98831168bc5d719b81993452383ca2bd653f5f
Arthur Kalikiti <arthur@crystaline.co.zm>
2019-01-18 09:07:43 +0200

    added the product offerings page

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -16,58 +16,7 @@ $faker = \Faker\Factory::create();
 										Preview of Our Offerings
 									</p>
 								<div>
-									<!-- Accordion starts -->
-									<div class="panel-group" id="accordion-alt3">
-								 <!-- Panel. Use "panel-XXX" class for different colors. Replace "XXX" with color. -->
-								  <div class="panel">	
-									<!-- Panel heading -->
-									 <div class="panel-heading">
-										<h4 class="panel-title">
-										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseOne-alt3">
-											<i class="fa fa-angle-right"></i> FINANCIAL CONSULTANCY
-										  </a>
-										</h4>
-									 </div>
-									 <div id="collapseOne-alt3" class="panel-collapse collapse">
-										<!-- Panel body -->
-										<div class="panel-body">
-										{{ $faker->paragraph }}
-										</div>
-									 </div>
-								  </div>
-								  <div class="panel">
-									 <div class="panel-heading">
-										<h4 class="panel-title">
-										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseTwo-alt3">
-											<i class="fa fa-angle-right"></i>SOFTWARE & VOLUME LICENSING
-										  </a>
-										</h4>
-									 </div>
-									 <div id="collapseTwo-alt3" class="panel-collapse collapse">
-										<div class="panel-body">
-										{{ $faker->paragraph }}
-										</div>
-									 </div>
-								  </div>
-								  <div class="panel">
-									 <div class="panel-heading">
-										<h4 class="panel-title">
-										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseThree-alt3">
-											<i class="fa fa-angle-right"></i> BUSINESS INTELLIGENCE
-										  </a>
-										</h4>
-									 </div>
-									 <div id="collapseThree-alt3" class="panel-collapse collapse">
-										<div class="panel-body">
-										{{ $faker->paragraph }}
-										</div>
-									 </div>
-								  </div>
-							
-								<!-- Accordion ends -->
 								
-							</div>
-							
 
 								</div>  
 							</div>
