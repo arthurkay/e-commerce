b0bab673136348cf38341ca59a506ab90d9bbee1
Arthur Kalikiti <arthurkalikiti@gmail.com>
2019-01-29 23:07:21 +0200

    Removed prefix on checkout route group

--- routes/web.php
+++ routes/web.php
@@ -62,7 +62,7 @@ Route::group(['prefix' => 'admin',  'middleware' => ['auth']], function ()
 
 Auth::routes(['verify' => true, 'register' => false]);
 
-Route::group(['middleware' => 'auth', 'prefix' => 'checkout'], function() {
+Route::group(['middleware' => 'auth'], function() {
 	Route::get('/home', 'MainController@user')->name('user');
 	Route::get('{id}/{product}', 'MainController@checkout')->name('checkout');
 	Route::get('/cart/{id}/{product}', 'MainController@cart')->name('cart');
